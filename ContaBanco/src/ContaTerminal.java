3bddf8539cc87653e13358bbbe9a2f7f3041cdce
paulagvp <paulagvp@gmail.com>
2023-10-16 15:29:14 -0300

    Desafio realizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,9 +1,25 @@
-public class ContaTerminal {
-    
-    String contaCliente
-    int numeroConta
-    String nomeCliente
-    double saldo
+import java.util.Locale;
+import java.util.Scanner;
 
+public class ContaTerminal{
+    public static void main(String[] args) {
+        try (//criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Por favor, digite o seu primeiro nome: ");
+            String nomeCliente = scanner.next();
+            
+            System.out.println("Por favor, digite o número da sua Agência: ");
+            String agencia = scanner.next();
 
+            System.out.println("Por favor, digite o número da sua conta: ");
+            int numero = scanner.nextInt();
+            
+            System.out.println("Por favor, digite o seu saldo: (utilizando . para a parte decimal) ");
+            double saldo = scanner.nextDouble();
+            
+            //imprimindo os dados obtidos pelo usuario
+            System.out.println("Olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo de " + saldo + " reais, já está disponível para saque.");
+        }
+        
+    }
 }
